e59098d31dd032b4cf59170b7986410bbcbeec68
TeamGuidemic <teamguidemic@gmail.com>
2023-03-12 11:28:59 +0530

    Initial dist subtree commit

---
+++ precache-manifest.c3627067feace69944418305b15a2c70.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef81eb7ffc2324139137",
+    "url": "/vue-pages/css/app.b8a1228b.css"
+  },
+  {
+    "revision": "cee2a5ff723b2be86ef8d4b9ec49aca9",
+    "url": "/vue-pages/img/logo-banner.cee2a5ff.jpeg"
+  },
+  {
+    "revision": "7c9193bd5f5f22a733861db4cae921f0",
+    "url": "/vue-pages/img/logo-no-bg.7c9193bd.png"
+  },
+  {
+    "revision": "4db2a0596d882ef212655f048ff2adcc",
+    "url": "/vue-pages/index.html"
+  },
+  {
+    "revision": "99711223c8c17061643c",
+    "url": "/vue-pages/js/about.34576eae.js"
+  },
+  {
+    "revision": "ef81eb7ffc2324139137",
+    "url": "/vue-pages/js/app.37ab7226.js"
+  },
+  {
+    "revision": "69907deecc6cb48fab10",
+    "url": "/vue-pages/js/chunk-vendors.309c1bb7.js"
+  },
+  {
+    "revision": "c685c348b9dc4009ce9ff6d8be56cd90",
+    "url": "/vue-pages/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pages/robots.txt"
+  }
+]);
